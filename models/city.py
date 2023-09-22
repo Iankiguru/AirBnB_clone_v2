999653783c150ada028c40a010a358fe0efac6db
Gmwongela <gvidla73@gmail.com>
2023-09-22 18:18:02 +0000

    updated

--- models/city.py
+++ models/city.py
@@ -1,26 +1,9 @@
 #!/usr/bin/python3
-"""
-City class module.
-"""
-
+""" City Module for HBNB project """
 from models.base_model import BaseModel
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
-from os import environ
 
-class City(BaseModel, Base):
-    """
-    City class that inherits from BaseModel and Base.
-    """
 
-    if "HBNB_TYPE_STORAGE" in environ and environ["HBNB_TYPE_STORAGE"] == "db":
-        __tablename__ = 'cities'
-
-        name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-
-    # Add a relationship to State
-    state = relationship("State", back_populates="cities")
-
-    # Define a one-to-many relationship from City to Place
-    places = relationship('Place', back_populates='cities', cascade='all, delete-orphan')
+class City(BaseModel):
+    """ The city class, contains state ID and name """
+    state_id = ""
+    name = ""
--- models/state.py
+++ models/state.py
@@ -1,20 +1,8 @@
 #!/usr/bin/python3
-"""
-State class module.
-"""
-
+""" State Module for HBNB project """
 from models.base_model import BaseModel
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
 
-class State(BaseModel, Base):
-    """
-    State class that inherits from BaseModel and Base.
-    """
-    
-    __tablename__ = 'states'
-    
-    name = Column(String(128), nullable=False)
-    
-    # Add a relationship to City
-    cities = relationship("City", back_populates="state", cascade="all, delete-orphan")
+
+class State(BaseModel):
+    """ State class """
+    name = ""
